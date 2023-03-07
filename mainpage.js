25295df72189b3f675bc63cc5a088a3eddd5f3e8
Jason Lewis <pensfan5050@gmail.com>
2023-03-06 19:43:27 -0500

    second commit

--- mainpage.js
+++ mainpage.js
@@ -1,18 +1,24 @@
-$(document).ready(function(){
-    $('#autocomplete').on('input', function(){
+
+fetch("https://api.edamam.com/api/nutrition-data?app_id=2197e613&app_key=49154045da302b5e11705006dfe3dfa6&ingr=1%20apple")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+ $(document).ready(function(){
+    $(document).on('#food-input', function(){
         const query = $(this).val();
         $.ajax({
-            url:"https://api.edamam.com/api/food-database/v2/parser",
+            url:"https://api.edamam.com/api/nutrition-data?app_id=2197e613&app_key=49154045da302b5e11705006dfe3dfa6&ingr=1%20apple",
+
             data:{
                 q : query
             },
             success: function(response){
                 const suggestions = response.results.map(result => result.name);
-                const dropdown =$('<div id = "autocomplete-dropdown"></div>');
+                const dropdown =$('<div id = "food-list"></div>');
                 suggestions.forEach(suggestion => {
-                    const item =$('<div id = "autocomplete-dropdown"></div>').text(suggestion);
+                    const item =$('<div id = "food-list"></div>').text(suggestion);
                     item.on(click,function(){
-                        $('#autocomplete').val(suggestion);
+                        $('#food-input').val(suggestion);
                         dropdown.hide();
                     });
                     dropdown.append(item);
@@ -21,6 +27,6 @@ $(document).ready(function(){
             }
         });
     });
-});
+}); 
 
 //"https://code.jquery.com/jquery-3.6.0.min.js"
