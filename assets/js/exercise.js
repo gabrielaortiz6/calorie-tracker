40e2e826283648e7b3df47568393ab6b7ddef04f
David Rodriguez <davidrodriguez@Davids-MacBook-Pro.local>
2023-03-07 19:36:13 -0500

    deleted extra vscode file and andded nameInput

--- assets/js/exercise.js
+++ assets/js/exercise.js
@@ -1,5 +1,5 @@
 // API key for calories burned API
-var exerciseAPIKey = 'BN9muUJAIB/KbJBS1hPo0g==sL04lBW2RLHAQr7Q';
+var exerciseAPIKey = "BN9muUJAIB/KbJBS1hPo0g==sL04lBW2RLHAQr7Q";
 
 //var exercise has to be user input - must target the element
 var exercise = "";
@@ -8,5 +8,36 @@ var exercise = "";
 var duration = "";
 
 // exercise and duration will be parameters
-var exerciseAPIUrl = 'https://api.api-ninjas.com/v1/caloriesburned?activity=' + exercise + '&duration=' + duration;
+var exerciseAPIUrl =
+  "https://api.api-ninjas.com/v1/caloriesburned?activity=" +
+  exercise +
+  "&duration=" +
+  duration;
 
+//
+// window.onload = function () {
+//   var nameElement = document.getElementById("user-name");
+//   nameElement.innerHTML = "David";
+// };
+// function updateName() {
+//   var nameElement = document.getElementById("name");
+//   var nameInput = document.getElementById("nameInput");
+//   nameElement.innerHTML = nameInput.value;
+// }
+//
+function updateName() {
+  var nameInput = document.getElementById("nameInput");
+  localStorage.setItem("name", nameInput.value);
+  updateNameElement();
+}
+function updateNameElement() {
+  var nameElement = document.getElementById("name");
+  var name = localStorage.getItem("name");
+  if (name) {
+    nameElement.innerHTML = name;
+  }
+}
+
+window.onload = function () {
+  updateNameElement();
+};
