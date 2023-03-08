5bca232bc95a03ef16e5abc393f943db526a5cc7
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-03-07 21:44:11 -0500

    calories are now displaying properly

--- assets/js/exercise.js
+++ assets/js/exercise.js
@@ -12,6 +12,43 @@ var exercisesArray = [];
 
 $(document).ready(function () {
 
+    // function for drop down list
+    // function autocomplete() {
+    //     var availableExercises= [
+
+    //       "Baseball",
+    //       "Basketball",
+    //       "Boxing",
+    //       "Climbing",
+    //       "Dancing",
+    //       "Football",
+    //       "Frisbee",
+    //       "Golf",
+    //       "Handball",
+    //       "High"
+    //       "Horseback riding",
+    //       "Lifting",
+    //       "Martial Arts",
+    //       "Ping Pong",
+    //       "Pushup",
+    //       "Run",
+    //       "Sailing",
+    //       "Situp",
+    //       "Skii",
+    //       "Soccer",
+    //       "Surfing",
+    //       "Tennis",
+    //       "Track",
+    //       "Volleyball",
+    //       "Walking",
+    //       "Water skii"
+    //     ];
+
+    // //     $( "#tags" ).autocomplete({
+    // //       source: availableExercises
+    // //     });
+    // //   };
+
     //function for retrieving user input from storage
     function retrieveStorage() {
 
@@ -25,47 +62,56 @@ $(document).ready(function () {
 
         //loop through each object in the exercisesArray
         exercisesArray.forEach((obj) => {
-
-            // Create a new table row
-            var newRow = document.createElement('tr');
-            newRow.classList.add("table-row");
-            var exerciseCell = document.createElement('td');
-            var durationCell = document.createElement('td');
-            var caloriesBurnedCell = document.createElement('td')
-            caloriesBurnedCell.classList.add('calories-burned');
-
-            // Add the data from local storage to the new row
-            exerciseCell.textContent = obj.exercise;
-            durationCell.textContent = obj.duration;
-            caloriesBurnedCell.textContent = "";
-
-            //Append text content to new row
-            newRow.appendChild(exerciseCell);
-            newRow.appendChild(durationCell);
-            newRow.appendChild(caloriesBurnedCell);
-
-            // Check if the data is already in the table (source: Stack Overflow)
-            var exerciseInTable = Array.from(exerciseTable.querySelectorAll('td:first-child'))
-                .map((cell) => cell.textContent)
-                .includes(obj.exercise);
-
-            // Skip adding the data if it already exists
-            if (exerciseInTable) {
-                return;
-            }
-            // Append the new row to the table
-            exerciseTable.appendChild(newRow);
+            addRow(obj.exercise, obj.duration, obj.calories);
         });
     };
 
-//function to append fetched data to table
-    function appendCaloriesBurned (data) {
+    function addRow(exercise, duration, calories) {
+           // Create a new table row
+           var newRow = document.createElement('tr');
+           newRow.classList.add("table-row");
+           var exerciseCell = document.createElement('td');
+           var durationCell = document.createElement('td');
+           var caloriesBurnedCell = document.createElement('td');
+           caloriesBurnedCell.classList.add('calories-burned');
 
-        //target cells we created
+           // Add the data from local storage to the new row
+           exerciseCell.textContent = exercise;
+           durationCell.textContent = duration;
+           caloriesBurnedCell.textContent = calories;
+
+           //Append text content to new row
+           newRow.appendChild(exerciseCell);
+           newRow.appendChild(durationCell);
+           newRow.appendChild(caloriesBurnedCell);
+
+           // Check if the data is already in the table (source: Stack Overflow)
+           var exerciseInTable = Array.from(exerciseTable.querySelectorAll('td:first-child'))
+               .map((cell) => cell.textContent)
+               .includes(exercise);
+
+           // Skip adding the data if it already exists
+           if (exerciseInTable) {
+               return;
+           }
+
+           // Append the new row to the table
+           exerciseTable.appendChild(newRow);
+    }
+
+    //function to append fetched data to table
+    function appendCaloriesBurned(data) {
+
         var caloriesBurnedCell = $('.calories-burned');
+        //target cells we created
+        caloriesBurnedCell.each(function () {
 
-        //set value of new cell to parameter
-        caloriesBurnedCell.text(data);
+            if (caloriesBurnedCell.text() === "") {
+                //set value of new cell to parameter
+                caloriesBurnedCell.text(data);
+            }
+
+        });
     }
 
     //function for processing and storing the inputs
@@ -77,27 +123,18 @@ $(document).ready(function () {
         var duration = userDurationInput.val();
 
         if (exercise && duration) {
+            //retrieves storage
             getCaloriesBurnedApi(exercise, duration);
-            //  console.log(exercise + " " + duration)
+            //console.log(exercise + " " + duration)
         } else if (!exercise || !duration || exercise == " " || duration == " ") {
             window.alert("Error! Please properly enter the name of your exercise activity and the duration");
             return;
         }
 
-        //stack overflow helped me here. creating objects out of inputs to add to the empty array
-        exercisesArray.push({ exercise: exercise, duration: duration });
-
-        console.log(exercisesArray);
-
-        //sets local storage with array
-        localStorage.setItem("user inputs", JSON.stringify(exercisesArray));
-
         //empties the user input area after submitting input with click
         userExerciseInput.val("");
         userDurationInput.val("");
 
-        //call other functions
-        retrieveStorage();
     }
 
     // exercise and duration will be parameters
@@ -122,7 +159,15 @@ $(document).ready(function () {
             console.log(data);
             var caloriesBurned = data[0].total_calories;
             console.log(caloriesBurned);
-            appendCaloriesBurned(caloriesBurned);
+             //stack overflow helped me here. creating objects out of inputs to add to the empty array
+             exercisesArray.push({ exercise: exercise, duration: duration, calories: caloriesBurned });
+
+             console.log(exercisesArray);
+ 
+             //sets local storage with array
+             localStorage.setItem("user inputs", JSON.stringify(exercisesArray));
+             retrieveStorage();
+            //appendCaloriesBurned(caloriesBurned);
         });
     };
 
